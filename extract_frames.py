ac37a83ac4e9b45928da1bebd46cae4cba0c366d
bhavi187 <vbhavishya05@gmail.com>
2025-03-01 14:09:15 -0500

    Update extract_frames.py

--- extract_frames.py
+++ extract_frames.py
@@ -1,13 +1,13 @@
 import cv2
 
 def extract_frames(video_path):
-    """Extracts frames from the given video."""
+    
     cap = cv2.VideoCapture(video_path)
 
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
-            break  # Stop when video ends
-        yield frame  # Return frame one by one
+            break  
+        yield frame 
 
     cap.release()
